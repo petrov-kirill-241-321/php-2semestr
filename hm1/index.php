c8d33a9ff52d031440b70ff7e4642057db743ee3
Kirill <kurumi8927@gmail.com>
2025-05-30 16:02:31 +0300

    hm1

---
+++ hm1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Петров Кирилл Андреевич 241-321 Домашняя работа №1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <img src="./access/logo.jpg" alt="logo">
+        <h1 class="heading">2.1. Домашняя работа: Hello, World!</h1>
+    </header>
+
+    <main>
+        <p><?php echo "Hello, World!" ?></p>
+        <p>
+            <?php echo date("l") ?>
+        </p> 
+    </main>
+
+    <footer>
+        <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. </p>
+    </footer>
+</body>
+
+</html>
